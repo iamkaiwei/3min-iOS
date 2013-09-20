1ded4f846864cd36b7dfcaf1cd60ace28830129f
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-09-21 00:52:50 +0700

    - Add login page.

---
+++ Controllers/Login Page/TMELoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMELoginViewController.h
+//  PhotoButton
+//
+//  Created by Triệu Khang on 21/9/13.
+//
+//
+
+#import "TMEBaseViewController.h"
+
+@interface TMELoginViewController : TMEBaseViewController
+
+@end
---
+++ Controllers/Login Page/TMELoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  TMELoginViewController.m
+//  PhotoButton
+//
+//  Created by Triệu Khang on 21/9/13.
+//
+//
+
+#import "TMELoginViewController.h"
+
+@interface TMELoginViewController ()
+
+@end
+
+@implementation TMELoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
