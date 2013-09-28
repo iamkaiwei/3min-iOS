4df9ce478e2eaa506081da50a3ee6b0d483a36de
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-09-29 00:43:29 +0700

    - Add icons into Tabbar.

--- Controllers/Main Controller/TMEViewController.m
+++ Controllers/Main Controller/TMEViewController.m
@@ -37,14 +37,40 @@
     TMEBrowserProductsViewController *dummy1 = [[TMEBrowserProductsViewController alloc] init];
     TMEBrowserProductsViewController *dummy2 = [[TMEBrowserProductsViewController alloc] init];
     
-    UITabBar *tabBar = self.tabBarController.tabBar;
-    UITabBarItem *browserBtn = [tabBar.items objectAtIndex:0];
-    UITabBarItem *dummy1Btn = [tabBar.items objectAtIndex:1];
-    UITabBarItem *publishBtn = [tabBar.items objectAtIndex:2];
-    UITabBarItem *loginBtn = [tabBar.items objectAtIndex:3];
-    UITabBarItem *dummy2Btn = [tabBar.items objectAtIndex:4];
-    
     self.viewControllers = @[browserVC, dummy1, publishVC, loginVC, dummy2];
+    
+    UITabBar *tabBar = self.tabBar;
+    
+    // browser button
+    UITabBarItem *browserBtn = [tabBar.items objectAtIndex:0];
+    browserVC.view.frame = CGRectMake(browserVC.view.frame.origin.x, browserVC.view.frame.origin.y + 1, browserVC.view.frame.size.width, browserVC.view.frame.size.height);
+    browserBtn.title = @"";
+    UIImage *browserBtnBackground = [UIImage imageNamed:@"tabbar-browser-icon"];
+    [browserBtn setFinishedSelectedImage:browserBtnBackground withFinishedUnselectedImage:browserBtnBackground];
+    
+    // search dummy button
+    UITabBarItem *dummy1Btn = [tabBar.items objectAtIndex:1];
+    dummy1Btn.title = @"";
+    UIImage *dummy1BtnBtnBackground = [UIImage imageNamed:@"tabbar-search-icon"];
+    [dummy1Btn setFinishedSelectedImage:dummy1BtnBtnBackground withFinishedUnselectedImage:dummy1BtnBtnBackground];
+    
+    // publish button
+    UITabBarItem *publishBtn = [tabBar.items objectAtIndex:2];
+    publishBtn.title = @"";
+    UIImage *publish1BtnBackground = [UIImage imageNamed:@"tabbar-sell-icon"];
+    [publishBtn setFinishedSelectedImage:publish1BtnBackground withFinishedUnselectedImage:publish1BtnBackground];
+    
+    // login button
+    UITabBarItem *loginBtn = [tabBar.items objectAtIndex:3];
+    loginBtn.title = @"";
+    UIImage *loginBtnBackground = [UIImage imageNamed:@"tabbar-activity-icon"];
+    [loginBtn setFinishedSelectedImage:loginBtnBackground withFinishedUnselectedImage:loginBtnBackground];
+    
+    // me button
+    UITabBarItem *dummy2Btn = [tabBar.items objectAtIndex:4];
+    dummy2Btn.title = @"";
+    UIImage *dummy2BtnBackground = [UIImage imageNamed:@"tabbar-me-icon"];
+    [dummy2Btn setFinishedSelectedImage:dummy2BtnBackground withFinishedUnselectedImage:dummy2BtnBackground];
 }
 
 #pragma mark - Link to product config
