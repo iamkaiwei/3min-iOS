cf53dc47bc42697a0468754addebd26404454611
Hoang Trieu Khang <hoangtrieukhang@gmail.com>
2013-09-28 14:55:10 +0700

    - Add the buttons to navigation bar.

--- Controllers/Superclass/TMEBaseViewController.m
+++ Controllers/Superclass/TMEBaseViewController.m
@@ -52,6 +52,8 @@
     self.tapToDismissKeyboardGestureRecognizer.numberOfTapsRequired = 1;
     self.tapToDismissKeyboardGestureRecognizer.numberOfTouchesRequired = 1;
     [[self getScrollableView] addGestureRecognizer:self.tapToDismissKeyboardGestureRecognizer];
+    
+    [self addNavigationItems];
 }
 
 - (void)viewDidUnload
@@ -436,6 +438,44 @@
 - (void)handleNotification:(NSNotification *)notification
 {
     
+}
+
+#pragma marks - Some VC stuffs
+- (void)addNavigationItems
+{
+    // Nav left button
+    UIButton *leftButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    if (self == [[self.navigationController viewControllers] objectAtIndex:0]) {
+        [leftButton setImage:[UIImage imageNamed:@"category-list-icon"] forState:UIControlStateNormal];
+//        [leftButton addTarget:self.viewDeckController action:@selector(toggleLeftView) forControlEvents:UIControlEventTouchUpInside];
+    }else{
+        [leftButton setImage:[UIImage imageNamed:@"category-list-icon"] forState:UIControlStateNormal];
+        [leftButton addTarget:self action:@selector(onBtnBack) forControlEvents:UIControlEventTouchUpInside];
+    }
+    
+    leftButton.frame = CGRectMake(0, 0, 40, 30);
+    UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftButton];
+    self.navigationItem.leftBarButtonItem = leftButtonItem;
+    
+    // Nav right button
+    UIButton *rightButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [rightButton setImage:[UIImage imageNamed:@"grid-view-icon"] forState:UIControlStateNormal];
+//    [rightButton addTarget:self.viewDeckController action:@selector(toggleRightView) forControlEvents:UIControlEventTouchUpInside];
+    rightButton.frame = CGRectMake(0, 0, 40, 30);
+    UIBarButtonItem *rightButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightButton];
+    self.navigationItem.rightBarButtonItem = rightButtonItem;
+}
+
+- (void)onBtnBack
+{
+    [self.navigationController popToRootViewControllerAnimated:YES];
+}
+
+- (void)trackCritercismBreadCrumb:(NSUInteger)lineNumber
+{
+    NSString *breadcrumb = [NSString stringWithFormat:@"%@:%d", [[NSString stringWithUTF8String:__FILE__] lastPathComponent], lineNumber];
+    [Crittercism leaveBreadcrumb:breadcrumb];
 }
 
 @end
