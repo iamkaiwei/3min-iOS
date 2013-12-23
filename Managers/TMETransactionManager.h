4a8e5846db66bad34f291c8953452565c37fb5ca
Toan Slan <batoan1221@gmail.com>
2013-12-23 20:04:59 +0700

    Building get chat message list

---
+++ Managers/TMETransactionManager.h
@@ -0,0 +1,13 @@
+//
+//  TMETransactionManager.h
+//  PhotoButton
+//
+//  Created by Toan Slan on 12/23/13.
+//
+//
+
+#import "BaseManager.h"
+
+@interface TMETransactionManager : BaseManager
+
+@end
---
+++ Managers/TMETransactionManager.m
@@ -0,0 +1,13 @@
+//
+//  TMETransactionManager.m
+//  PhotoButton
+//
+//  Created by Toan Slan on 12/23/13.
+//
+//
+
+#import "TMETransactionManager.h"
+
+@implementation TMETransactionManager
+
+@end
