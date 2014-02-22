63c1f864816b400cfa7e34696ed40774d4b7f9a9
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-02-23 00:59:16 +0700

    fix font of navigation bar.

--- Controllers/Navigation Controller/TMENavigationViewController.m
+++ Controllers/Navigation Controller/TMENavigationViewController.m
@@ -19,21 +19,28 @@
   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
   if (self) {
     // Custom initialization
-      [[UINavigationBar appearance] setTitleTextAttributes:
-       @{ UITextAttributeTextColor: [UIColor whiteColor],
-          UITextAttributeTextShadowColor: [UIColor clearColor],
-          UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
-          UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Thin" size:25.0f] }];
-    
       self.navigationBar.frame = CGRectMake(0, 0, 320, 44);
     if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
       self.navigationBar.barStyle = UIBarStyleBlackTranslucent;
       self.navigationBar.translucent = YES;
       self.navigationBar.barTintColor = [UIColor orangeMainColor];
       self.navigationBar.tintColor = [UIColor whiteColor];
+        [[UINavigationBar appearance] setTitleTextAttributes:
+         @{ UITextAttributeTextColor: [UIColor whiteColor],
+            UITextAttributeTextShadowColor: [UIColor clearColor],
+            UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
+            UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Thin" size:25.0f]
+            }];
+        return self;
     }
+      [[UINavigationBar appearance] setTitleTextAttributes:
+       @{ UITextAttributeTextColor: [UIColor whiteColor],
+          UITextAttributeTextShadowColor: [UIColor clearColor],
+          UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
+          UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Light" size:25.0f]
+          }];
   }
-  
+
   return self;
 }
 
