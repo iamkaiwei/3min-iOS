6a1eeaae0e205ff49bfa68d71659a7ff82ef42ce
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-02-23 15:33:00 +0700

    Browser product cell header

--- PhotoButton/Controllers/Browser Products/TMEBrowserProductTableViewHeader.h
+++ PhotoButton/Controllers/Browser Products/TMEBrowserProductTableViewHeader.h
@@ -9,10 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface TMEBrowserProductTableViewHeader : UITableViewHeaderFooterView
-@property (weak, nonatomic) IBOutlet UIImageView *imgUserAvatar;
-@property (weak, nonatomic) IBOutlet UILabel *lblUserName;
-@property (weak, nonatomic) IBOutlet UILabel *lblTimestamp;
 
 - (void)configHeaderWithData:(TMEProduct *)product;
 + (CGFloat)getHeight;
+
 @end
--- PhotoButton/Controllers/Browser Products/TMEBrowserProductTableViewHeader.m
+++ PhotoButton/Controllers/Browser Products/TMEBrowserProductTableViewHeader.m
@@ -8,12 +8,34 @@
 
 #import "TMEBrowserProductTableViewHeader.h"
 
+@interface TMEBrowserProductTableViewHeader()
+
+@property (weak, nonatomic) IBOutlet UIImageView *imgUserAvatar;
+@property (weak, nonatomic) IBOutlet UILabel *lblUserName;
+@property (weak, nonatomic) IBOutlet UILabel *lblTimestamp;
+
+@property (weak, nonatomic) IBOutlet UIView *whiteBackgroundForIOS6;
+@property (weak, nonatomic) IBOutlet UIToolbar *blurBackground;
+
+@end
+
 @implementation TMEBrowserProductTableViewHeader
 
 - (void)configHeaderWithData:(TMEProduct *)product{
   [self.imgUserAvatar setImageWithURL:[NSURL URLWithString:product.user.photo_url] placeholderImage:nil];
   self.lblUserName.text = product.user.fullname;
   self.lblTimestamp.text = [product.created_at relativeDate];
+}
+
+- (void)layoutSubviews
+{
+    [super layoutSubviews];
+
+    if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_6_1) {
+        self.blurBackground.hidden = YES;
+        self.whiteBackgroundForIOS6.backgroundColor = [UIColor whiteColor];
+        self.whiteBackgroundForIOS6.alpha = 0.8;
+    }
 }
 
 + (CGFloat)getHeight{
