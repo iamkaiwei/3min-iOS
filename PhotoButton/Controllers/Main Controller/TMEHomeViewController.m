4640d55d412789675f32657ab23e033243c3917b
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-02-24 10:29:48 +0700

    Correct the tabbar background

--- PhotoButton/Controllers/Main Controller/TMEHomeViewController.m
+++ PhotoButton/Controllers/Main Controller/TMEHomeViewController.m
@@ -47,27 +47,28 @@ UIImagePickerControllerDelegate
 
 - (void)viewDidLoad
 {
-  [super viewDidLoad];
-  self.title = @"Main Menu";
-  
-  [self.tabBar setBackgroundImage:[UIImage imageNamed:@"tabbar-background"]];
-  self.viewControllers = @[];
-  
-  [self addBrowserProductTab];
-  [self addSearchButton];
-  [self addPublishButton];
-  [self addActivityButton];
-  [self addMeButton];
-  
-  if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
-//    [self.view enableOrangeStatusBar];
-    self.statusBarView = [[UIView alloc] initWithFrame:[[UIApplication sharedApplication] statusBarFrame]];
-    [self.statusBarView setBackgroundColor:[UIColor orangeMainColor]];
-    [self.statusBarView setAlpha:0.0];
-    [self.view addSubview:self.statusBarView];
-    return;
-  }
-  [self.tabBar setSelectionIndicatorImage:[[UIImage alloc] init]];
+    [super viewDidLoad];
+    self.title = @"Main Menu";
+
+    self.viewControllers = @[];
+
+    [self addBrowserProductTab];
+    [self addSearchButton];
+    [self addPublishButton];
+    [self addActivityButton];
+    [self addMeButton];
+
+    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
+        self.statusBarView = [[UIView alloc] initWithFrame:[[UIApplication sharedApplication] statusBarFrame]];
+        [self.statusBarView setBackgroundColor:[UIColor orangeMainColor]];
+        [self.statusBarView setAlpha:0.0];
+        [self.view addSubview:self.statusBarView];
+        
+        return;
+    }
+
+    [self.tabBar setBackgroundImage:[UIImage imageNamed:@"tabbar-background"]];
+    [self.tabBar setSelectionIndicatorImage:[[UIImage alloc] init]];
 }
 
 #pragma marks - UI helper
@@ -90,12 +91,12 @@ UIImagePickerControllerDelegate
 - (void)createTabbarButtonWithIconName:(NSString *)iconName
                                atIndex:(NSInteger)index
 {
-  UITabBar *tabBar = self.tabBar;
-  UITabBarItem *button = [tabBar.items objectAtIndex:index];
-  button.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
-  UIImage *browserBtnBackground = [UIImage imageNamed:iconName];
-  UIImage *browserSelectedBtnBackground = [UIImage imageNamed:[NSString stringWithFormat:@"%@-pressed", iconName]];
-  [button setFinishedSelectedImage:browserSelectedBtnBackground withFinishedUnselectedImage:browserBtnBackground];
+    UITabBar *tabBar = self.tabBar;
+    UITabBarItem *button = [tabBar.items objectAtIndex:index];
+    button.imageInsets = UIEdgeInsetsMake(6, 0, -6, 0);
+    UIImage *browserBtnBackground = [UIImage imageNamed:iconName];
+    UIImage *browserSelectedBtnBackground = [UIImage imageNamed:[NSString stringWithFormat:@"%@-pressed", iconName]];
+    [button setFinishedSelectedImage:browserSelectedBtnBackground withFinishedUnselectedImage:browserBtnBackground];
 }
 
 - (void)addBrowserProductTab
