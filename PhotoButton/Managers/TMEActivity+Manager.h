93518f139616ddd79a62c1e5fd58aab6703b8df5
Trieu Khang <hoangtrieukhang@gmnail.com>
2014-04-26 16:28:36 +0700

    Add Activity Manager

---
+++ PhotoButton/Managers/TMEActivity+Manager.h
@@ -0,0 +1,13 @@
+//
+//  TMEActivity+Manager.h
+//  ThreeMin
+//
+//  Created by Triệu Khang on 25/4/14.
+//
+//
+
+#import "TMEActivity.h"
+
+@interface TMEActivity (Manager)
+
+@end
---
+++ PhotoButton/Managers/TMEActivity+Manager.m
@@ -0,0 +1,19 @@
+//
+//  TMEActivity+Manager.m
+//  ThreeMin
+//
+//  Created by Triệu Khang on 25/4/14.
+//
+//
+
+#import "TMEActivity+Manager.h"
+
+@implementation TMEActivity (Manager)
+
++ (void)getActivitySuccess:(void(^)(NSArray *arrActivities))successBlock failure:(void(^)(NSError *error))failureBlock {
+
+
+
+}
+
+@end
