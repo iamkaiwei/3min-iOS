89f0387ddc5db423de8b4fca0720b274cde49ab6
khanghoang <hoangtrieukhang@gmail.com>
2014-08-04 03:03:13 +0700

    Add home page view datasource

--- 3min-iOS/Controllers/Home Page VC/TMEHomePageViewDatasource.m
+++ 3min-iOS/Controllers/Home Page VC/TMEHomePageViewDatasource.m
@@ -8,6 +8,56 @@
 
 #import "TMEHomePageViewDatasource.h"
 
+@interface TMEHomePageViewDatasource()
+
+@property (strong, nonatomic) TMEHomePageViewController *pageVC;
+
+@end
+
 @implementation TMEHomePageViewDatasource
+
+- (TMESearchPageContentViewController *)searchVC {
+    if (!_searchVC) {
+        _searchVC = [[TMESearchPageContentViewController alloc] init];
+    }
+    return _searchVC;
+}
+
+- (TMEBrowserPageContentViewController *)browserVC {
+    if (!_browserVC) {
+        _browserVC = [[TMEBrowserPageContentViewController alloc] init];
+    }
+    return _browserVC;
+}
+
+- (TMEProfilePageContentViewController *)profileVC {
+    if (!_profileVC) {
+        _profileVC = [[TMEProfilePageContentViewController alloc] init];
+    }
+
+    return _profileVC;
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
+
+    NSDictionary *controllers = @{
+                                  NSStringFromClass([TMESearchPageContentViewController class]): [NSNull null],
+                                  NSStringFromClass([TMEBrowserPageContentViewController class]): self.searchVC,
+                                  NSStringFromClass([TMEProfilePageContentViewController class]): self.browserVC,
+                                  };
+
+    return controllers[NSStringFromClass([viewController class])] ?: nil;
+}
+
+- (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
+
+    NSDictionary *controllers = @{
+                                  NSStringFromClass([TMESearchPageContentViewController class]): self.browserVC,
+                                  NSStringFromClass([TMEBrowserPageContentViewController class]): self.profileVC,
+                                  NSStringFromClass([TMEProfilePageContentViewController class]): [NSNull null],
+                                  };
+
+    return controllers[NSStringFromClass([viewController class])] ?: nil;
+}
 
 @end
