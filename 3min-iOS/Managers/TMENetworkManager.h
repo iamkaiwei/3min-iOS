45a6eee0e3756d893dc7665fd8ebb3528640ed8c
Khoa Pham <onmyway133@gmail.com>
2014-08-04 22:41:09 +0700

    Add singleton macro LS macro

---
+++ 3min-iOS/Managers/TMENetworkManager.h
@@ -0,0 +1,15 @@
+//
+//  TMENetworkManager.h
+//  ThreeMin
+//
+//  Created by Khoa Pham on 8/3/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import "TMEBaseManager.h"
+
+@interface TMENetworkManager : TMEBaseManager
+
+OMNIA_SINGLETON_H(sharedManager)
+
+@end
---
+++ 3min-iOS/Managers/TMENetworkManager.m
@@ -0,0 +1,15 @@
+//
+//  TMENetworkManager.m
+//  ThreeMin
+//
+//  Created by Khoa Pham on 8/3/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import "TMENetworkManager.h"
+
+@implementation TMENetworkManager
+
+OMNIA_SINGLETON_M(sharedManager)
+
+@end
