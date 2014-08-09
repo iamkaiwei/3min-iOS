ee32035afd8af4281d985d95d296b0be7755a851
khanghoang <hoangtrieukhang@gmail.com>
2014-08-09 17:26:29 +0700

    Doing the new browser products

---
+++ 3min-iOS/Controllers/Home Page VC/Browser/TMEProductCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  TMEProductCollectionViewCell.h
+//  ThreeMin
+//
+//  Created by Triệu Khang on 9/8/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMEProductCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ 3min-iOS/Controllers/Home Page VC/Browser/TMEProductCollectionViewCell.m
@@ -0,0 +1,31 @@
+//
+//  TMEProductCollectionViewCell.m
+//  ThreeMin
+//
+//  Created by Triệu Khang on 9/8/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import "TMEProductCollectionViewCell.h"
+
+@implementation TMEProductCollectionViewCell
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
