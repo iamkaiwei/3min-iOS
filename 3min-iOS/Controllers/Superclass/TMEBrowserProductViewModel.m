f143cbbf87e58ba9664f5c7d4df93a25117b27dc
khanghoang <hoangtrieukhang@gmail.com>
2014-08-19 12:18:54 +0700

    Add state of view model

--- 3min-iOS/Controllers/Superclass/TMEBrowserProductViewModel.m
+++ 3min-iOS/Controllers/Superclass/TMEBrowserProductViewModel.m
@@ -11,6 +11,11 @@
 @interface TMEBrowserProductViewModel ()
 
 @property (copy, nonatomic, readwrite) NSArray *arrayItems;
+
+@property (nonatomic, readwrite) TMEViewModelState state;
+
+@property (nonatomic, readwrite) TMEPaginationCollectionViewDataSource *datasource;
+
 @property (assign, nonatomic) NSUInteger page;
 
 @end
@@ -21,9 +26,32 @@
 	self = [super init];
 	if (self) {
 		_page = 1;
+		self.state = TMEViewModelStateLoading;
 	}
 
 	return self;
+}
+
+- (id)initWithCollectionView:(UICollectionView *)collection {
+    self = [super init];
+    if (self) {
+    }
+    return self;
+}
+
+- (id <UICollectionViewDataSource> )dataSourceFactory:(TMEViewModelState)state {
+#warning NEED REFACTOR
+	return [[TMEPaginationCollectionViewDataSource alloc] initWithItems:self.arrayItems identifierParserBlock:nil configureCellBlock:nil];
+//	if (state == TMEViewModelStateLoadingMorePage) {
+//		return [[TMEPaginationCollectionViewDataSource alloc] init];
+//	}
+//
+//	return [[TMEPaginationCollectionViewDataSource alloc] init];
+}
+
+- (void)setState:(TMEViewModelState)state {
+	_state = state;
+	self.datasource = [self dataSourceFactory:state];
 }
 
 - (NSArray *)arrayItems {
@@ -35,17 +63,15 @@
 }
 
 - (void)getProducts:(void (^)(NSArray *arrProducts))success failure:(void (^)(NSError *error))failure withPage:(NSUInteger)page {
-
-    if (self.page == -1) {
-        return;
-    }
+	if (self.page == -1) {
+		return;
+	}
 
 	[TMEProductsManager getAllProductsWihPage:page
 	                           onSuccessBlock: ^(NSArray *arrProducts) {
-
 	    if (arrProducts.count == 0) {
 	        self.page = -1;
-            return;
+	        return;
 		}
 
 	    NSMutableArray *arr = [self.arrayItems mutableCopy];
@@ -53,8 +79,9 @@
 	    self.arrayItems = arr;
 	    self.page++;
 
+	    self.state = TMEViewModelStateLoadingMorePage;
 	} failureBlock: ^(NSError *error) {
-
+	    self.state = TMEViewModelStateError;
 	}];
 }
 
