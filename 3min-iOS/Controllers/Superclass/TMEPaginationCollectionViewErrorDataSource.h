c34596672f83ca1e882826401a93433a9bee4c63
khanghoang <hoangtrieukhang@gmail.com>
2014-08-19 17:40:37 +0700

    Add error data source

---
+++ 3min-iOS/Controllers/Superclass/TMEPaginationCollectionViewErrorDataSource.h
@@ -0,0 +1,14 @@
+//
+//  TMEPaginationCollectionViewErrorDataSource.h
+//  ThreeMin
+//
+//  Created by Triệu Khang on 19/8/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "TMEBrowserProductViewModel.h"
+
+@interface TMEPaginationCollectionViewErrorDataSource : NSObject
+
+@end
