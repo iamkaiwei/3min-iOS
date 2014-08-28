44b0abc87f3d1a0e69d729b304b404b83d4ba68e
khanghoang <hoangtrieukhang@gmail.com>
2014-08-28 10:11:16 +0700

    Display the menu view controller

--- 3min-iOS/Controllers/Home Page VC/TMEHomeNavigationButtonDelegate.m
+++ 3min-iOS/Controllers/Home Page VC/TMEHomeNavigationButtonDelegate.m
@@ -8,43 +8,70 @@
 
 #import "TMEHomeNavigationButtonDelegate.h"
 
-@interface TMEHomeNavigationButtonDelegate()
+@interface TMEHomeNavigationButtonDelegate ()
 
 @property (weak, nonatomic) TMEPageViewController *pageViewController;
 @property (weak, nonatomic) TMENavigationViewController *navViewController;
+
+@property (strong, nonatomic) UIViewController *backgroundVC;
+@property (assign, nonatomic) BOOL isShowingMenu;
 
 @end
 
 @implementation TMEHomeNavigationButtonDelegate
 
 - (id)init {
-    NSAssert(NO, @"Have to call initWithPageViewController:navigationViewController:");
-    self = [self initWithPageViewController:nil navigationViewController:nil];
-    return nil;
+	NSAssert(NO, @"Have to call initWithPageViewController:navigationViewController:");
+	self = [self initWithPageViewController:nil navigationViewController:nil];
+	return nil;
 }
 
 - (instancetype)initWithPageViewController:(TMEPageViewController *)pageVC navigationViewController:(TMENavigationViewController *)navVC {
-    NSParameterAssert(pageVC);
-    NSParameterAssert(navVC);
-    self = [super init];
-    if (self) {
-        _pageViewController = pageVC;
-        _navViewController = navVC;
-    }
+	NSParameterAssert(pageVC);
+	NSParameterAssert(navVC);
+	self = [super init];
+	if (self) {
+		_pageViewController = pageVC;
+		_navViewController = navVC;
+	}
 
-    return self;
+	return self;
 }
 
 - (void)onTapSearchButton:(UIButton *)sender {
-    [self.pageViewController goToSearchViewController];
+	[self.pageViewController goToSearchViewController];
 }
 
 - (void)onTapProfileButton:(UIButton *)sender {
-    [self.pageViewController goToProfileViewController];
+	[self.pageViewController goToProfileViewController];
 }
 
 - (void)onTapTitleButton:(UIButton *)sender {
-    [self.pageViewController goToBrowserProductViewController];
+	[self.pageViewController goToBrowserProductViewController];
+	[self tongleMenu];
+}
+
+- (void)tongleMenu {
+	if (self.isShowingMenu == YES) {
+		[self.backgroundVC.view removeFromSuperview];
+		[self.backgroundVC removeFromParentViewController];
+		self.isShowingMenu = NO;
+		return;
+	}
+
+	self.isShowingMenu = YES;
+	self.backgroundVC = [[UIViewController alloc] init];
+
+	self.backgroundVC.view.frame = ({
+	                                    CGRect frame = self.navViewController.view.bounds;
+	                                    frame.origin = CGPointMake(0, 64);
+	                                    frame;
+									});
+
+	self.backgroundVC.view.backgroundColor = [UIColor grayColor];
+	[self.navViewController addChildViewController:self.backgroundVC];
+	[self.navViewController.view addSubview:self.backgroundVC.view];
+	[self.backgroundVC didMoveToParentViewController:self.navViewController];
 }
 
 @end
