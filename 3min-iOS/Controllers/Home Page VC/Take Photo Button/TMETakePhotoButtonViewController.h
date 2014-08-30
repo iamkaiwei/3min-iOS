c1b79e1b8639904f2890eee7948e9c09344c62a8
khanghoang <hoangtrieukhang@gmail.com>
2014-08-30 18:56:27 +0700

    Add Take photo button

---
+++ 3min-iOS/Controllers/Home Page VC/Take Photo Button/TMETakePhotoButtonViewController.h
@@ -0,0 +1,13 @@
+//
+//  TMETakePhotoButtonViewController.h
+//  ThreeMin
+//
+//  Created by Triệu Khang on 30/8/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TMETakePhotoButtonViewController : UIViewController
+
+@end
---
+++ 3min-iOS/Controllers/Home Page VC/Take Photo Button/TMETakePhotoButtonViewController.m
@@ -0,0 +1,27 @@
+//
+//  TMETakePhotoButtonViewController.m
+//  ThreeMin
+//
+//  Created by Triệu Khang on 30/8/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import "TMETakePhotoButtonViewController.h"
+
+@interface TMETakePhotoButtonViewController ()
+
+@end
+
+@implementation TMETakePhotoButtonViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (IBAction)onTapTakePhoto:(id)sender {
+    DLog(@"Take photo");
+}
+
+@end
