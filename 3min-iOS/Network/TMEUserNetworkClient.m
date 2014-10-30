e9dfb3a8c86eba31c3da1be4e99739d04f2111b4
khanghoang <hoangtrieukhang@gmail.com>
2014-10-30 14:37:23 +0700

    Fix logged user save

--- 3min-iOS/Network/TMEUserNetworkClient.m
+++ 3min-iOS/Network/TMEUserNetworkClient.m
@@ -14,92 +14,86 @@
 
 #pragma mark - Public Interface
 - (void)loginWithFacebookWithSuccess:(TMESuccessBlock)success
-                             failure:(TMEFailureBlock)failure
-{
-    [[TMEFacebookManager sharedManager] signInWithSuccess:^(NSString *facebookToken) {
-        [self loginWithFacebookToken:facebookToken
-                             success:success
-                             failure:failure];
-    } failure:^(NSError *error) {
-
-    }];
+                             failure:(TMEFailureBlock)failure {
+	[[TMEFacebookManager sharedManager] signInWithSuccess: ^(NSString *facebookToken) {
+	    [self loginWithFacebookToken:facebookToken
+	                         success:success
+	                         failure:failure];
+	} failure: ^(NSError *error) {
+	}];
 }
 
-
 - (void)loginWithGooglePlusWithSuccess:(TMESuccessBlock)success
-                               failure:(TMEFailureBlock)failure
-{
-    [[TMEGooglePlusManager sharedManager] signInWithSuccess:^(NSString *googlePlusToken)
-     {
-         [self loginWithGooglePlusToken:googlePlusToken
-                                success:success
-                                failure:failure];
-     } failure:^(NSError *error) {
-         
-     }];
+                               failure:(TMEFailureBlock)failure {
+	[[TMEGooglePlusManager sharedManager] signInWithSuccess: ^(NSString *googlePlusToken)
+	{
+	    [self loginWithGooglePlusToken:googlePlusToken
+	                           success:success
+	                           failure:failure];
+	}                                               failure: ^(NSError *error) {
+	}];
 }
 
 #pragma mark - Core
 - (void)loginWithFacebookToken:(NSString *)facebookToken
                        success:(TMESuccessBlock)success
-                       failure:(TMEFailureBlock)failure
-{
-    NSDictionary *params = @{@"fb_token": facebookToken,
-                             };
+                       failure:(TMEFailureBlock)failure {
+	NSDictionary *params = @{ @"fb_token": facebookToken, };
 
-    [self loginWithParams:params success:success failure:failure];
+	[self loginWithParams:params success:success failure:failure];
 }
 
 - (void)loginWithGooglePlusToken:(NSString *)googlePlusToken
                          success:(TMESuccessBlock)success
-                         failure:(TMEFailureBlock)failure
-{
-    NSDictionary *params = @{@"gg_token": googlePlusToken
-                             };
+                         failure:(TMEFailureBlock)failure {
+	NSDictionary *params = @{ @"gg_token": googlePlusToken };
 
-    [self loginWithParams:params success:success failure:failure];
+	[self loginWithParams:params success:success failure:failure];
 }
 
 - (void)loginWithParams:(NSDictionary *)params
                 success:(TMESuccessBlock)success
-                failure:(TMEFailureBlock)failure
-{
-    NSDictionary *identityParams = @{
-                                     @"udid": [TMEDeviceManager sharedManager].UDID,
-                                     @"client_secret": API_CLIENT_SERCET,
-                                     @"client_id": API_CLIENT_ID,
-                                     @"grant_type": API_GRANT_TYPE
-                                     };
+                failure:(TMEFailureBlock)failure {
+	NSDictionary *identityParams = @{
+		@"udid": [TMEDeviceManager sharedManager].UDID,
+		@"client_secret": API_CLIENT_SERCET,
+		@"client_id": API_CLIENT_ID,
+		@"grant_type": API_GRANT_TYPE
+	};
 
-    NSMutableDictionary *combinedParams = [NSMutableDictionary dictionaryWithDictionary:identityParams];
-    [combinedParams addEntriesFromDictionary:params];
+	NSMutableDictionary *combinedParams = [NSMutableDictionary dictionaryWithDictionary:identityParams];
+	[combinedParams addEntriesFromDictionary:params];
 
-    NSString *path = [NSString stringWithFormat:@"%@%@", API_BASE_URL, API_USER_LOGIN];
+	NSString *path = [NSString stringWithFormat:@"%@%@", API_BASE_URL, API_USER_LOGIN];
 
-    [[TMENetworkManager sharedManager] post:path
-                                     params:combinedParams
-                                    success:^(id responseObject)
-    {
-        TMEUser *user = [TMEUser tme_modelFromJSONResponse:responseObject[@"user"]];
-        user.accessTokenReceivedAt = [NSDate date];
+	[[TMENetworkManager sharedManager] post:path
+	                                 params:combinedParams
+	                                success: ^(id responseObject)
+	{
+        // put every information at the same hierarchy level
+	    NSMutableDictionary *dicInfo = [NSMutableDictionary dictionaryWithDictionary:responseObject];
+        [dicInfo addEntriesFromDictionary:responseObject[@"user"]];
 
-        [TMEUserManager sharedManager].loggedUser = user;
-        [[TMEUserManager sharedManager] save];
+	    TMEUser *user = [TMEUser tme_modelFromJSONResponse:dicInfo];
+	    user.accessTokenReceivedAt = [NSDate date];
 
-        [[TMENetworkManager sharedManager] updateAuthorizationHeader];
+	    [TMEUserManager sharedManager].loggedUser = user;
+	    [[TMEUserManager sharedManager] save];
 
-        if (success) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                success();
-            });
-        }
-    } failure:^(NSError *error) {
-        if (failure) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                failure(error);
-            });
-        }
-    }];
+	    [[TMENetworkManager sharedManager] updateAuthorizationHeader];
+
+	    if (success) {
+	        dispatch_async(dispatch_get_main_queue(), ^{
+	            success();
+			});
+		}
+	}                               failure: ^(NSError *error) {
+	    if (failure) {
+	        dispatch_async(dispatch_get_main_queue(), ^{
+	            failure(error);
+			});
+		}
+	}];
 }
 
 @end
