df5ac5212eda3f00f38eab11a27f1b132591533b
khanghoang <hoangtrieukhang@gmail.com>
2014-11-03 15:15:22 +0700

    Change when tap on other profile

--- 3min-iOS/Controllers/Home Page VC/Profile/TMEProfilePageContentViewController.m
+++ 3min-iOS/Controllers/Home Page VC/Profile/TMEProfilePageContentViewController.m
@@ -8,6 +8,7 @@
 
 #import "TMEProfilePageContentViewController.h"
 #import "TMEMyTopProfileViewController.h"
+#import "TMEOtherTopProfileViewController.h"
 #import "TMEListActiviesViewController.h"
 
 @interface TMEProfilePageContentViewController ()
@@ -19,60 +20,89 @@
 
 @implementation TMEProfilePageContentViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+	if (self) {
+		// Custom initialization
+	}
+	return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view from its nib.
 
-    TMEMyTopProfileViewController *topVC = [[TMEMyTopProfileViewController alloc] init];
-    topVC.user = self.user;
-    [self addChildViewController:topVC];
-    [topVC willMoveToParentViewController:self];
-    [self.view addSubview:topVC.view];
-    [topVC didMoveToParentViewController:self];
+	UIViewController *topVC = [self _getTopViewController];
+	[self addChildViewController:topVC];
+	[topVC willMoveToParentViewController:self];
+	[self.view addSubview:topVC.view];
+	[topVC didMoveToParentViewController:self];
+	self.topVC = topVC;
 
-    self.topVC = topVC;
+    UIViewController *activitiesVC = [self _getBottomController];
+	[self addChildViewController:activitiesVC];
+	[activitiesVC willMoveToParentViewController:self];
+	[self.view addSubview:activitiesVC.view];
+	[activitiesVC didMoveToParentViewController:self];
 
-    TMEListActiviesViewController *activitiesVC = [[TMEListActiviesViewController alloc] init];
-    [self addChildViewController:activitiesVC];
-    [activitiesVC willMoveToParentViewController:self];
-    [self.view addSubview:activitiesVC.view];
-    [activitiesVC didMoveToParentViewController:self];
+	self.activitiesVC = activitiesVC;
 
-    self.activitiesVC = activitiesVC;
-
-    [self.view layoutIfNeeded];
+	[self.view layoutIfNeeded];
 }
 
 - (void)updateViewConstraints {
-    [super updateViewConstraints];
+	[super updateViewConstraints];
 
-    [self.topVC.view mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.view).with.offset(0);
-        make.height.equalTo(@160);
-        make.leading.equalTo(self.view);
-        make.trailing.equalTo(self.view);
-    }];
+	[self.topVC.view mas_remakeConstraints: ^(MASConstraintMaker *make) {
+	    make.top.equalTo(self.view).with.offset(0);
+	    make.height.equalTo(@160);
+	    make.leading.equalTo(self.view);
+	    make.trailing.equalTo(self.view);
+	}];
 
-    [self.view layoutIfNeeded];
+	[self.view layoutIfNeeded];
 
-    [self.activitiesVC.view mas_remakeConstraints:^(MASConstraintMaker *make) {
-        make.bottom.equalTo(self.view).with.offset(0);
-        make.top.equalTo(self.view).with.offset(160);
-        make.leading.equalTo(self.view);
-        make.trailing.equalTo(self.view);
-    }];
+	[self.activitiesVC.view mas_remakeConstraints: ^(MASConstraintMaker *make) {
+	    make.bottom.equalTo(self.view).with.offset(0);
+	    make.top.equalTo(self.view).with.offset(160);
+	    make.leading.equalTo(self.view);
+	    make.trailing.equalTo(self.view);
+	}];
 
-    [self.view layoutIfNeeded];
+	[self.view layoutIfNeeded];
+}
+
+#pragma mark - private
+
+- (BOOL)_isLoggedInUserProfile {
+	if ([self.user.userID isEqual:[[[TMEUserManager sharedManager] loggedUser] userID]]) {
+		return YES;
+	}
+
+	return NO;
+}
+
+- (UIViewController *)_getTopViewController {
+	if ([self _isLoggedInUserProfile]) {
+		// show my own profile
+		TMEMyTopProfileViewController *topVC = [[TMEMyTopProfileViewController alloc] init];
+		topVC.user = self.user;
+		return topVC;
+	}
+
+	// and the orther
+	TMEOtherTopProfileViewController *topVC = [[TMEOtherTopProfileViewController alloc] init];
+	return topVC;
+}
+
+- (UIViewController *)_getBottomController {
+	if ([self _isLoggedInUserProfile]) {
+		TMEListActiviesViewController *activitiesVC = [[TMEListActiviesViewController alloc] init];
+		return activitiesVC;
+	}
+
+	TMEListActiviesViewController *activitiesVC = [[TMEListActiviesViewController alloc] init];
+	return activitiesVC;
 }
 
 @end
