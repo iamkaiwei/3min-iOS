0f791a724340dc04aef5b0a646d1c64c785d0ddf
Khoa Pham <onmyway133@gmail.com>
2014-11-18 16:03:06 +0700

    Add edit product name vc

---
+++ 3min-iOS/Modules/Edit Product/Item/TMEEditProductNameVC.h
@@ -0,0 +1,13 @@
+//
+//  TMEEditProductNameVC.h
+//  ThreeMin
+//
+//  Created by Khoa Pham on 11/18/14.
+//  Copyright (c) 2014 3min. All rights reserved.
+//
+
+#import "TMEBaseViewController.h"
+
+@interface TMEEditProductNameVC : TMEBaseViewController
+
+@end
