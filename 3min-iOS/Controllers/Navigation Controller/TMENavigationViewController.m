76876ddb79931326a04e81b3e1a0165762662c0f
Khoa Pham <onmyway133@gmail.com>
2014-11-18 15:05:44 +0700

    Fix navigation bar

--- 3min-iOS/Controllers/Navigation Controller/TMENavigationViewController.m
+++ 3min-iOS/Controllers/Navigation Controller/TMENavigationViewController.m
@@ -19,26 +19,31 @@
   self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
   if (self) {
     // Custom initialization
-      self.navigationBar.frame = CGRectMake(0, 0, 320, 44);
-    if (SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(@"7.0")) {
+
+    if (IS_IOS7_OR_ABOVE) {
       self.navigationBar.barStyle = UIBarStyleBlackTranslucent;
-      self.navigationBar.translucent = YES;
-      self.navigationBar.barTintColor = [UIColor orangeMainColor];
+      //self.navigationBar.translucent = YES;
+      self.navigationBar.barTintColor = [UIColor redColor];;
       self.navigationBar.tintColor = [UIColor whiteColor];
+
+        // TODO: Needs to fix this navigation bar
+//        [[UINavigationBar appearance] setTitleTextAttributes:
+//         @{ UITextAttributeTextColor: [UIColor whiteColor],
+//            UITextAttributeTextShadowColor: [UIColor clearColor],
+//            UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
+//            UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Thin" size:25.0f]
+//            }];
+
+    } else {
+        //self.navigationBar.frame = CGRectMake(0, 0, 320, 44);
         [[UINavigationBar appearance] setTitleTextAttributes:
          @{ UITextAttributeTextColor: [UIColor whiteColor],
             UITextAttributeTextShadowColor: [UIColor clearColor],
             UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
-            UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Thin" size:25.0f]
+            UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Light" size:22.0f]
             }];
-        return self;
     }
-      [[UINavigationBar appearance] setTitleTextAttributes:
-       @{ UITextAttributeTextColor: [UIColor whiteColor],
-          UITextAttributeTextShadowColor: [UIColor clearColor],
-          UITextAttributeTextShadowOffset: [NSValue valueWithUIOffset:UIOffsetMake(0.0f, 0.0f)],
-          UITextAttributeFont: [UIFont fontWithName:@"HelveticaNeue-Light" size:22.0f]
-          }];
+
   }
 
   return self;
